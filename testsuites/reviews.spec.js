b4174b6000d0bff55e526d1a34ef145d4d42e004
dchiu91 <d.chiu91@yahoo.com>
2022-09-22 20:25:10 -0700

    create test suite

---
+++ testsuites/reviews.spec.js
@@ -0,0 +1,14 @@
+
+describe("Example tests", function () {
+
+    // Individual tests can be run using the "it" or "test" methods, they are aliased and are equivalent
+    it("1 should be 1", function () {
+        /* This test suite is written in Jest. There are many more methods other than "toBe"
+        Go to: https://jestjs.io/docs/en/expect
+        to find more options if "toBe" doesn't fit your use case.
+        */
+        expect(1).toBe(1);
+    });
+
+});
+
