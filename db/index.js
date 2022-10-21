58da27b5d55a86684ef077f1fac684df0b23f739
dchiu91 <d.chiu91@yahoo.com>
2022-10-20 21:21:10 -0700

    working with bobazon FEC

--- db/index.js
+++ db/index.js
@@ -5,121 +5,250 @@ const csv = require('csv-parser')
 const fs = require('fs')
 
 
-const { Client } = require('pg')
+const { Pool } = require('pg')
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  "max": 20,
+  "connectionTimeoutMillis": 100,
+  "idleTimeoutMillis": 100
+});
 
-
-let createTempTable =
-  `
-CREATE TABLE TEMPTABLE (
-  id INTEGER NOT NULL,
-  url TEXT,
-  PRIMARY KEY(ID)
-);
-`
-let dropTempTable =
-  `
-DROP TABLE TEMPTABLE;
-`
-
-
-
-/*
-summary TEXT,
-body TEXT,
-recommend BOOLEAN,
-  reported BOOLEAN,
-  reviewer_name TEXT,
-  reviewer_email TEXT,
-  response TEXT,
-  helpfulness INT,
-
-  */
-
-
-const getReviews = (page,count,sort,product_id) => {
+const getReviews = (page, count, sort, product_id) => {
 
   let getReviewsQuery =
     `
-SELECT
-  id,
-  product_id,
-  rating,
-  summary,
-  recommend,
-  response,
-  body,
-  date,
-  reviewer_name,
-  helpfulness,
-  array_remove(array_agg(rjp.url),NULL) as photos
+    SELECT
+      id,
+      product_id,
+      rating,
+      summary,
+      recommend,
+      response,
+      body,
+      date,
+      reviewer_name,
+      helpfulness,
+      array_remove(array_agg(rjp.url),NULL) as photos
 
-FROM
-(SELECT a.*, b.url
-  FROM reviews as a
-  LEFT JOIN reviews_photos as b
-  ON a.id = b.review_id
-  WHERE product_id = ${product_id}
-  order by ${sort} desc
-  limit ${count}
-  ) as rjp
+    FROM
+      (
+      SELECT a.*, b.url
+      FROM reviews as a
+      LEFT JOIN reviews_photos as b
+      ON a.id = b.review_id
+      WHERE product_id = ${product_id}
+      ) as rjp
 
-  GROUP BY rjp.id, rjp.product_id, rjp.rating, rjp.date, rjp.summary, rjp.body, rjp.recommend,
-  rjp.reported, rjp.reviewer_name, rjp.reviewer_email, rjp.response, rjp.helpfulness
+      GROUP BY rjp.id, rjp.product_id, rjp.rating, rjp.date, rjp.summary, rjp.body, rjp.recommend,
+      rjp.reported, rjp.reviewer_name, rjp.reviewer_email, rjp.response, rjp.helpfulness
 
-LIMIT 10;
-`;
+    ${sort}
+    LIMIT ${count} OFFSET ${count * page};
+    `;
 
-  const client = new Client({
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
-  });
-  client.connect()
-    .then(() => {
-      console.log('connected');
-    })
-    .catch(err => {
-      console.error('error', err);
-    });
 
-  return client.query(getReviewsQuery)
+
+  return pool.query(getReviewsQuery)
     .then((data) => {
+      console.log(data.rows);
       return data.rows;
     })
     .catch((error) => {
       console.log('error with query', error);
     })
-    .finally(() => {
-
-      client.end()
-        .then(() => {
-          console.log('disconnected');
-        })
-        .catch((err) => {
-          console.log('error disconnecting');
-        });
-
-    })
 
 
 }
-// Promise.resolve(client.query(getReviewsQuery))
-//   .then((res) => {
-//     console.log('SUCCESS', res.rows);
-//   })
-//   .catch((err) => {
-//     console.log('ERROR', err);
-//   })
-//   .then(() => {
-//     console.log('done  with getReviews');
-//     client.end();
-//   });
 
-// }
+const getRatingsDistr = product_id => {
 
-// getReviews();
+
+  let getRatingsDistrQuery =
+    `
+  SELECT rating,count(*) as ratings_Count
+  FROM reviews
+  WHERE product_id = ${product_id}
+  GROUP BY rating;
+  `;
+
+
+  return pool.query(getRatingsDistrQuery)
+    .then((data) => {
+      // console.log(data.rows);
+      return data.rows;
+    })
+    .catch((error) => {
+      console.log('error with query', error);
+    })
+
+}
+
+const getRecommendedCount = product_id => {
+
+
+  let getRatingsDistrQuery =
+    `
+  SELECT count(id) as recommend_Count
+  FROM reviews
+  WHERE product_id = ${product_id} AND recommend = 'true'
+  `;
+
+
+  return pool.query(getRatingsDistrQuery)
+    .then((data) => {
+      // console.log(data.rows);
+      return data.rows;
+    })
+    .catch((error) => {
+      console.log('error with query', error);
+    })
+
+}
+
+
+const getCharacteristics = product_id => {
+
+
+  let getCharacteristicsQuery =
+    `
+  SELECT a.name, a.id, AVG(b.value)
+  FROM characteristics a
+  LEFT JOIN characteristic_reviews b
+  ON a.id = b.characteristic_id
+  WHERE product_id = ${product_id}
+  GROUP BY name, a.id;
+  `;
+
+
+  return pool.query(getCharacteristicsQuery)
+    .then((data) => {
+      // console.log(data.rows);
+      return data.rows;
+    })
+    .catch((error) => {
+      console.log('error with query', error);
+    })
+
+}
+
+
+const insertReview = (product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, photos, characteristics) => {
+
+  let insertReviewQuery =
+    `
+  INSERT INTO reviews (id,product_id,rating,summary,body,recommend,reviewer_name,reviewer_email,helpfulness)
+  VALUES (nextval('reviews_id_seq'), ${product_id}, ${rating}, '${summary}', '${body}', ${recommend}, '${reviewer_name}', '${reviewer_email}', 0)
+  RETURNING id;
+  `;
+
+  return pool.query(insertReviewQuery)
+
+    .then((res) => {
+      let reviewId = res.rows[0].id;
+
+      for (var i = 0; i < photos.length; i++) {
+
+        let insertPhotoQuery =
+          `
+          INSERT INTO reviews_photos (id,review_id,url)
+          VALUES (nextval('reviews_photos_id_seq'), '${reviewId}', '${photos[i]}');
+          `;
+        pool.query(insertPhotoQuery)
+          .catch(err => {
+            console.log(err);
+          });
+
+
+      }
+
+      let keys = Object.keys(characteristics)
+      for (var i = 0; i < keys.length ; i++) {
+
+        let insertCharacteristicQuery =
+          `
+          INSERT INTO characteristic_reviews (id, characteristic_id,review_id,value)
+          VALUES (nextval('characteristic_reviews_id_seq'), '${keys[i]}', '${reviewId}', '${characteristics[keys[i]]}');
+          `;
+        pool.query(insertCharacteristicQuery)
+          .catch(err => {
+            console.log(err);
+          });
+
+
+      }
+
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+
+
+const incrementHelpful = review_id => {
+
+
+  let incrementHelpfulQuery =
+    `
+    UPDATE reviews
+    SET helpfulness = helpfulness+1
+    WHERE id=${review_id};
+  `;
+
+
+  return pool.query(incrementHelpfulQuery)
+    .then((data) => {
+      // console.log(data.rows);
+      return data.rows;
+    })
+    .catch((error) => {
+      console.log('error with query', error);
+    })
+
+}
+
+const reportReview = review_id => {
+
+
+  let reportReviewQuery =
+    `
+    UPDATE reviews
+    SET reported = true
+    WHERE id=${review_id};
+  `;
+
+
+  return pool.query(reportReviewQuery)
+    .then((data) => {
+      // console.log(data.rows);
+      return data.rows;
+    })
+    .catch((error) => {
+      console.log('error with query', error);
+    })
+
+}
+
+// test();
+
+module.exports.getReviews = getReviews;
+module.exports.getRatingsDistr = getRatingsDistr;
+module.exports.getRecommendedCount = getRecommendedCount;
+module.exports.getCharacteristics = getCharacteristics;
+module.exports.insertReview = insertReview;
+module.exports.incrementHelpful = incrementHelpful;
+module.exports.reportReview = reportReview;
+
+
+// getRatingsDistr(2);
+// getRecommendedCount(2);
+// getCharacteristics(2);
+
 /*
+CHARACTERISTIC_REVIEWS
 id,characteristic_id,review_id,value
 1,1,1,4
 2,2,1,3
@@ -141,7 +270,7 @@ id,characteristic_id,review_id,value
 18,10,9,4
 19,11,9,3
 
-
+CHARACTERISTICS
 id,product_id,name
 1,1,"Fit"
 2,1,"Length"
@@ -211,4 +340,3 @@ id,review_id,url
 
 
 
-module.exports.getReviews = getReviews;
--- server/index.js
+++ server/index.js
@@ -1,27 +1,88 @@
 const db = require('../db/index');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+// For parsing application/json
+app.use(express.json());
+
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/reviews/meta*', (req, res) => {
+  console.log('meta');
+  let product_id = req.query.product_id;
+
+  let response = {};
+  response.product = product_id;
 
 
-app.get('/reviews/*', (req, res) => {
-  // console.log('id', req.params.id);
-  console.log("All query strings: " + JSON.stringify(req.query));
+  Promise.all([
+    db.getRatingsDistr(product_id),
+    db.getRecommendedCount(product_id),
+    db.getCharacteristics(product_id)
+  ])
+    .then((results) => {
+      let ratings = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+      };
+      results[0].map((result) => {
+        ratings[result['rating']] = Number(result['ratings_count']);
+        response.ratings = ratings;
+      })
+
+      response['recommended'] = { '0': results[1][0]['recommend_count'] };
+
+      // console.log(results[2]);
+
+      let characteristics = {};
+      results[2].map((result) => {
+        // console.log(result['avg']);
+        characteristics[result['name']] = { 'id': result['id'], 'value': result['avg'] };
+      })
+      response['characteristics'] = characteristics;
+      console.log(response);
+      res.send(response);
+    })
+
+
+
+
+})
+
+app.get('/reviews*', (req, res) => {
+  console.log('id', req.params.id);
+  // console.log("All query strings: " + JSON.stringify(req.query));
   let product_id = req.query.product_id;
   let page = req.query.page || 0;
   let count = req.query.count || 5;
   let sort;
-  if (req.query.sort === 'helpful' || req.query.sort === 'relevant') {
-    sort = req.query.sort;
-  } else {
-    sort = 'date';
+
+  switch (req.query.sort) {
+    case 'newest':
+      sort = 'ORDER BY date DESC';
+      break;
+    case 'helpful':
+      sort = 'ORDER BY helpfulness DESC'
+      break;
+    case 'relevant':
+      sort = 'ORDER BY helpfulness DESC, date DESC';
+      break;
+    default:
+      sort = ''
   }
 
   console.log('page, count, sort, id', page, count, sort, product_id);
 
-  const data = Promise.resolve(db.getReviews(page, count, sort, product_id))
+  Promise.resolve(db.getReviews(page, count, sort, product_id))
     .then((data) => {
       for (var i = 0; i < data.length; ++i) {
+        data[i].date = new Date(Number(data[i].date));
+
         let photosJSONArray = [];
         if (data[i].photos.length > 0) {
           for (var j = 0; j < data[i].photos.length; j++) {
@@ -39,8 +100,82 @@ app.get('/reviews/*', (req, res) => {
       res.send(response);
 
     })
+    .catch(err => {
+      res.send(err);
+    });
 })
+
+
+app.post('/reviews', (req, res) => {
+  let review = req.body;
+  console.log('insert into review: ',review);
+  Promise.resolve(db.insertReview(review.product_id, review.rating, review.summary, review.body, review.recommend, review.name, review.email, review.photos, review.characteristics))
+    .then((response) => {
+      res.send(response);
+    });
+
+})
+
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  db.incrementHelpful(req.params.review_id)
+  .then(results => {
+    res.send(results);
+  })
+  .catch(err => {
+    res.send(err);
+  });
+
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+/*
+[
+  { rating: 2, ratings_count: '1' },
+  { rating: 3, ratings_count: '1' },
+  { rating: 4, ratings_count: '2' },
+  { rating: 5, ratings_count: '1' }
+]
+
+[ { recommend_count: '3' } ]
+
+[ { name: 'Quality', avg: '4.2000000000000000' } ]
+
+
+
+
+
+{
+  "product_id": "2",
+  "ratings": {
+    2: 1,
+    3: 1,
+    4: 2,
+    // ...
+  },
+  "recommended": {
+    0: 5
+    // ...
+  },
+  "characteristics": {
+    "Size": {
+      "id": 14,
+      "value": "4.0000"
+    },
+    "Width": {
+      "id": 15,
+      "value": "3.5000"
+    },
+    "Comfort": {
+      "id": 16,
+      "value": "4.0000"
+    },
+    // ...
+}
+
+*/
